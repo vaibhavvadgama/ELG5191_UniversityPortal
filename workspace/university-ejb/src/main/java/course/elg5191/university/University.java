e70e4a0124130aabc0c755ed435eadb2ac48f9b6
jmccaus <jmccaus@gmail.com>
2012-11-16 20:03:51 -0500

    - adding University.java, to hold enumeration constants

---
+++ workspace/university-ejb/src/main/java/course/elg5191/university/University.java
@@ -0,0 +1,55 @@
+package course.elg5191.university;
+
+public class University 
+{
+	public enum PrereqType
+	{
+		Course,
+		Experience,
+		Technology
+	}
+	
+	public enum ExamType
+	{
+		Assignment,
+		Project,
+		Midterm,
+		Final,
+		Quiz
+	}
+	
+	public enum ConductType
+	{
+		Inclass,
+		Takehome
+	}
+	
+	public enum ExamSubmissionStatus
+	{
+		Pass,
+		Fail
+	}
+	
+	public enum StudentApplicationStatus
+	{
+		DecisionPending,
+		Accepted,
+		Rejected,
+		OnHold
+	}
+	
+	public enum StudentCourseRegistrationStatus
+	{
+		Current,
+		Pass,
+		Fail,
+		Dropped
+	}
+	
+	public enum UserRole
+	{
+		Admin,
+		Professor,
+		Student
+	}
+}
