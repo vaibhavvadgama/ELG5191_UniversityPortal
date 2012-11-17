0e9b570741ed6bcbed283b6dc600a5311d578537
jmccaus <jmccaus@gmail.com>
2012-11-16 22:39:53 -0500

    StudentApplication.java:  - implemented, not tested

--- Workspace/university-ejb/src/main/java/course/elg5191/university/beans/entity/StudentApplication.java
+++ Workspace/university-ejb/src/main/java/course/elg5191/university/beans/entity/StudentApplication.java
@@ -1,10 +1,13 @@
 package course.elg5191.university.beans.entity;
 
 import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import course.elg5191.university.University.*;
 
 /**
  * @author jmccausl
@@ -13,8 +16,9 @@ import javax.persistence.Id;
  */
 
 @Entity
-public class StudentApplication {
-
+public class StudentApplication 
+{
+	//Declarations
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int recordId;
@@ -22,125 +26,142 @@ public class StudentApplication {
 	private String program;
 	private String department;
 	private int studentId;
-	private enum status;
+	private StudentApplicationStatus status;
 	private boolean isActive;
 	private Date createdDate;
 	private Date modifiedDate;
 
-	public StudentApplication(){
+	//Constructor
+	public StudentApplication()
+	{
 
 	}
 
-	public void finalize() throws Throwable {
-
-	}
-
-	public int getRecordId(){
-		return 0;
+	//Public Accessors
+	public int getRecordId()
+	{
+		return this.recordId;
 	}
 
 	/**
 	 * 
 	 * @param recordId
 	 */
-	public void setRecordId(int recordId){
-
+	public void setRecordId(int recordId)
+	{
+		this.recordId = recordId;
 	}
 
-	public int getYear(){
-		return 0;
+	public int getYear()
+	{
+		return this.year;
 	}
 
 	/**
 	 * 
 	 * @param year
 	 */
-	public void setYear(int year){
-
+	public void setYear(int year)
+	{
+		this.year = year;
 	}
 
-	public String getProgram(){
-		return "";
+	public String getProgram()
+	{
+		return this.program;
 	}
 
 	/**
 	 * 
 	 * @param program
 	 */
-	public void setProgram(String program){
-
+	public void setProgram(String program)
+	{
+		this.program = program;
 	}
 
-	public String getDepartment(){
-		return "";
+	public String getDepartment()
+	{
+		return this.department;
 	}
 
 	/**
 	 * 
 	 * @param department
 	 */
-	public void setDepartment(String department){
-
+	public void setDepartment(String department)
+	{
+		this.department = department;
 	}
 
-	public int getStudentId(){
-		return 0;
+	public int getStudentId()
+	{
+		return this.studentId;
 	}
 
 	/**
 	 * 
 	 * @param studentId
 	 */
-	public void setStudentId(int studentId){
-
+	public void setStudentId(int studentId)
+	{
+		this.studentId = studentId;
 	}
 
-	public enum getStatus(){
-		return null;
+	public StudentApplicationStatus getStatus()
+	{
+		return this.status;
 	}
 
 	/**
 	 * 
 	 * @param status
 	 */
-	public void setStatus(enum status){
-
+	public void setStatus(StudentApplicationStatus status)
+	{
+		this.status = status;
 	}
 
-	public boolean getIsActive(){
-		return false;
+	public boolean getIsActive()
+	{
+		return this.isActive;
 	}
 
 	/**
 	 * 
 	 * @param isActive
 	 */
-	public void setIsActive(boolean isActive){
-
+	public void setIsActive(boolean isActive)
+	{
+		this.isActive = isActive;
 	}
 
-	public Date getCreatedDate(){
-		return null;
+	public Date getCreatedDate()
+	{
+		return this.createdDate;
 	}
 
 	/**
 	 * 
 	 * @param createdDate
 	 */
-	public void setCreatedDate(Date createdDate){
-
+	public void setCreatedDate(Date createdDate)
+	{
+		this.createdDate = createdDate;
 	}
 
-	public Date getModifiedDate(){
-		return null;
+	public Date getModifiedDate()
+	{
+		return this.modifiedDate;
 	}
 
 	/**
 	 * 
 	 * @param modifiedDate
 	 */
-	public void setModifiedDate(Date modifiedDate){
-
+	public void setModifiedDate(Date modifiedDate)
+	{
+		this.modifiedDate = modifiedDate;
 	}
 
 }
