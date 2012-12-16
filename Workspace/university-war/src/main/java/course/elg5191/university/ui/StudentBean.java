99c0bf34925ddc449cdfb0cddbaf82f9d3870b78
jmccaus <jmccaus@gmail.com>
2012-12-15 20:13:28 -0500

    - overall navigation

--- Workspace/university-war/src/main/java/course/elg5191/university/ui/StudentBean.java
+++ Workspace/university-war/src/main/java/course/elg5191/university/ui/StudentBean.java
@@ -8,65 +8,31 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.event.ActionEvent;
 
-import course.elg5191.university.beans.entity.Student;
-import course.elg5191.university.beans.session.StudentSession;
+import course.elg5191.university.beans.entity.SystemUser;
+import course.elg5191.university.beans.session.SystemUserSession;
 
 @ManagedBean(name = "studentBean")
 @SessionScoped
 public class StudentBean {
 	@EJB
-	private StudentSession studentSess;
-	private String newStudentName;
-	private Date newStudentBirth;
-	private String searchStudentName;
-	private Student searchStudentResult;
-
-	public Student getSearchStudentResult() {
-		return searchStudentResult;
-	}
-
-	public void setSearchStudentResult(Student searchStudentResult) {
-		this.searchStudentResult = searchStudentResult;
-	}
-
-	public String getSearchStudentName() {
-		return searchStudentName;
-	}
-
-	public void setSearchStudentName(String searchStudentName) {
-		this.searchStudentName = searchStudentName;
-	}
-
-	public Date getNewStudentBirth() {
-		return newStudentBirth;
-	}
-
-	public void setNewStudentBirth(Date newStudentBirth) {
-		this.newStudentBirth = newStudentBirth;
-	}
-
-	public String getNewStudentName() {
-		return newStudentName;
-	}
-
-	public void setNewStudentName(String newStudentName) {
-		this.newStudentName = newStudentName;
-	}
-
-	public List<Student> getStudents() 
+	private SystemUserSession userSess;
+	private SystemUser user;
+	
+	//Public Methods
+	public void setUserId(String userId)
 	{
-		return studentSess.getAllStudents();
-		//return null;
+		System.out.println("setting the system user with id " + userId);
+		
+		this.user = userSess.getUserById(Integer.parseInt(userId));
 	}
-
-	public void createStudent(ActionEvent event) {
-		Student student = new Student();
-		student.setBirthDate(newStudentBirth);
-		student.setName(newStudentName);
-		studentSess.addStudent(student);
+	
+	public String getUserId()
+	{
+		System.out.println("getting the system user id");
+		if (user == null)
+			return "";
+		else
+			return Integer.toString(this.user.getId());
 	}
-
-	public void searchStudentName(ActionEvent event) {
-		searchStudentResult = studentSess.getStudentByName(searchStudentName);
-	}
+	
 }
